15cdd2e08369a6d89355087b59d7e11fed3783f1
ExecutiveMarshaller <brice_morel@hotmail.com>
2016-09-30 09:51:22 +0200

    Update user_service.js

--- GBriceCorp/src/main/webapp/static/js/service/user_service.js
+++ GBriceCorp/src/main/webapp/static/js/service/user_service.js
@@ -2,7 +2,7 @@
 
 App.factory('UserService', ['$http', '$q', function($http, $q){
 
-    var REST_SERVICE_URI = 'http://localhost:8080/SpringAngularStartProject/user/';
+    var REST_SERVICE_URI = 'http://localhost:8080/GestBank/user/';
 
     var factory = {
         fetchAllUsers: fetchAllUsers,
